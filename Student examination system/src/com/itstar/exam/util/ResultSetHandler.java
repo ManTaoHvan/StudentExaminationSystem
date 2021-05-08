7d1735ae88c0c33eedc9c0dda4e43a06071d1639
晓小 <55075193+ManTaoHvan@users.noreply.github.com>
2021-05-08 21:41:12 +0800

    Delete Student examination system directory

--- Student examination system/src/com/itstar/exam/util/ResultSetHandler.java
+++
@@ -1,8 +0,0 @@
-package com.itstar.exam.util;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public interface ResultSetHandler {
-	void handle(ResultSet res) throws SQLException;
-}
